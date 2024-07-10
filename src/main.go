e711deec7f8bae49bcc1fe1d0c7735350b0b8c0a
chadcat7 <namish@tuta.io>
2024-07-10 16:47:01 +0530

    extract colors

--- src/main.go
+++ src/main.go
@@ -7,9 +7,12 @@ import (
 	"image/color"
 	"image/jpeg"
 	"image/png"
+	"math"
 	"os"
 	"path"
 	"strings"
+
+	"github.com/esimov/stackblur-go"
 )
 
 func getImage(imagePath string) (img image.Image, err error) {
@@ -52,6 +55,52 @@ func randomString() string {
 	return fmt.Sprintf("%x", b)[2:11]
 }
 
+func save_image(img image.Image) (cache_path string, err error) {
+	cache_path = fmt.Sprintf("/tmp/%s.png", randomString())
+
+	f1, err := os.Create(cache_path)
+	if err != nil {
+		err = fmt.Errorf("failed to create file: %v", err)
+	}
+	defer f1.Close()
+
+	if err = png.Encode(f1, img); err != nil {
+		err = fmt.Errorf("failed to encode: %v", err)
+	}
+
+	return
+}
+
+func sliceImage(img image.Image, grid int) (tiles []image.Image) {
+	tiles = make([]image.Image, 0, grid*grid)
+
+	if cap(tiles) == 0 {
+		return
+	}
+
+	shape := img.Bounds()
+
+	fheight := float64(shape.Max.Y / int(grid))
+	fwidth := float64(shape.Max.X / int(grid))
+
+	height := int(math.Ceil(fheight))
+	width := int(math.Ceil(fwidth))
+
+	for y := shape.Min.Y; y+height <= shape.Max.Y; y += height {
+
+		for x := shape.Min.X; x+width <= shape.Max.X; x += width {
+
+			tile := img.(interface {
+				SubImage(r image.Rectangle) image.Image
+			}).SubImage(image.Rect(x, y, x+width, y+height))
+
+			tiles = append(tiles, tile)
+		}
+	}
+
+	return
+}
+
 func main() {
 
 	imageExists, _ := exists(os.Args[1])
@@ -62,14 +111,32 @@ func main() {
 
 	img, _ := getImage(os.Args[1])
 
-	bounds := img.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
-	rgbImg := image.NewRGBA(image.Rect(0, 0, width, height))
+	blurred_img, _ := stackblur.Process(img, 30)
 
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-			rgbImg.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
+	tiles := sliceImage(blurred_img, 4)
+
+	for _, tile := range tiles {
+		colors := make(map[color.RGBA]int)
+		bounds := tile.Bounds()
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			for x := bounds.Min.X; x < bounds.Max.X; x++ {
+				col := tile.At(x, y)
+				r, g, b, a := col.RGBA()
+				rgbaCol := color.RGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)}
+				colors[rgbaCol]++
+
+			}
 		}
+		var mostFrequentColor color.RGBA
+		maxCount := 0
+		for col, count := range colors {
+			if count > maxCount {
+				maxCount = count
+				mostFrequentColor = col
+			}
+		}
+		hexColor := fmt.Sprintf("#%02x%02x%02x", mostFrequentColor.R, mostFrequentColor.G, mostFrequentColor.B)
+		fmt.Println(mostFrequentColor, hexColor)
 	}
+
 }
